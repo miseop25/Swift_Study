b9a2bb9811c93b93f8fa60ce88ed9948115bcfa9
misseop25 <richard25@naver.com>
2020-02-17 00:57:12 +0900

    swift grammar test

---
+++ start_swift_programing.playground/Contents.swift
@@ -0,0 +1,82 @@
+import UIKit
+import Swift
+
+var str = "Hello, playground"
+
+dump(str)
+
+class classTest {
+    var name : String?
+    var age : Int!
+    var someThing : Any?
+}
+
+let ct = classTest()
+ct.name = "minseop"
+ct.someThing = 10
+//ct.age = 26
+dump(ct)
+
+// 배열 기본 연습
+var testArray : Array<Int> = Array<Int>()
+for i in 0..<10 {
+    testArray.append(i)
+}
+
+
+//testArray.remove(at: 0)
+testArray.reverse()
+testArray.removeLast()
+
+print(testArray)
+
+var strings : [String] = []
+strings.append("hello")
+strings.append("hi")
+strings.count
+
+strings.sort()
+
+//dict 기본 연습
+
+var anyDict : Dictionary<Int, String> = [Int: String]()
+
+anyDict[0] = "hello"
+anyDict[1] = "hi"
+
+if let a : Any = anyDict[0]{
+    print("a : ", a)
+}
+
+anyDict.removeValue(forKey: 0)
+anyDict.isEmpty
+
+print(anyDict)
+
+
+// 3항 연산의 이해, True 명 왼쪽꺼 False면 오른쪽 사용
+let contentHeight = 40
+let hasHeader = false
+let rowHeight = contentHeight + (contentHeight < 30  ? 50 : 20)
+
+var helloWorld : String = "Hello World!"
+helloWorld[helloWorld.startIndex]
+helloWorld[helloWorld.index(before: helloWorld.endIndex)]
+helloWorld[helloWorld.index(helloWorld.startIndex, offsetBy: 4)]
+helloWorld.insert(contentsOf: " swift", at: helloWorld.endIndex)
+
+let indexTest = helloWorld.index(of: "l") ?? helloWorld.endIndex
+let begining = helloWorld[indexTest...]
+helloWorld.removeAll()
+print(begining)
+
+
+type(of: begining)
+let newString = String(begining)
+type(of: newString)
+
+
+
+
+
+
