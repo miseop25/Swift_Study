e0193ce8a58509e418b9cd781719af52f9f23421
misseop25 <richard25@naver.com>
2020-11-18 00:18:17 +0900

    animate study 업데이트

--- AnimateStudy/AnimateStudy/ViewController.swift
+++ AnimateStudy/AnimateStudy/ViewController.swift
@@ -8,10 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var videoView: UIView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        videoView.alpha = 0
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animate(withDuration: 1,delay: 0.1, animations: ({
+            self.videoView.alpha = 1
+            self.videoView.frame = CGRect(x: 150, y: 100, width: 100, height: 100)
+
+            self.videoView.backgroundColor = UIColor.systemBlue
+        }))
+
+        print("alpa : ",self.videoView.alpha)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.videoView.backgroundColor = UIColor.black
+        self.videoView.frame = CGRect(x: 50, y: 50, width: 50, height: 50)
     }
 
 
