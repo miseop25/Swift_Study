02a7f961fc55d6d5125e46f0fdd81f65aca547f0
misseop25 <richard25@naver.com>
2020-12-02 23:46:18 +0900

    타입 캐스팅 공부

---
+++ My_example/TypeCasting_Example.playground/Contents.swift
@@ -0,0 +1,45 @@
+import Cocoa
+
+class Person {
+    let name: String
+    init(name : String) {
+        self.name = name
+    }
+}
+
+class Media {
+    let name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class Song : Media {
+    var artist: String
+    init(name: String, artist: String) {
+        self.artist = artist
+        super.init(name: name)
+    }
+}
+
+let mySong = Song(name: "노래", artist: "가수")
+if mySong is Media {
+    print("true")
+}else{
+    print("False")
+}
+
+let m = Media(name: "test")
+
+if let downCasting = m as? Song {
+    print(downCasting.name)
+}else {
+    print(false)
+}
+
+if let upCasting = mySong as? Media {
+    print("Upcasting 성공", upCasting.name)
+}else {
+    print("Upcasting 실패")
+}
+
